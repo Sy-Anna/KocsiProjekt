c80360f09013fb5374454f95f34ad33a1b23538a
Joseph867 <145647037+Joseph867@users.noreply.github.com>
2024-10-14 19:16:16 +0200

    Annának speciálba: tires oldal KINÉZET meg csinálval oszt jó va, style.css modosítva, extras.html ,color.html, fuel.html, main.ts are under development. tempcode az csak úgy van és kész. na peace

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './style.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const honda = document.querySelector('HondaJazz') as HTMLImageElement;
+    const inputColor = document.querySelector('ColorInput') as HTMLInputElement;
+    inputColor.addEventListener('input', () => {
+        honda.style.backgroundColor = inputColor.value;
+    })
+})
