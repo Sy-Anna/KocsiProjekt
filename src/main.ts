86abb7049bbc5f1b5faf01286150c753a9a565dd
Joseph867 <145647037+Joseph867@users.noreply.github.com>
2024-10-17 03:59:11 +0200

    annának speciálba 3: na szóval fuel cucc KINÉZET meg csinálva hála istennek color cucc is meg csinálva jó van az úgy, index cuccon iconok hozzá adva sztem nagyon jók de majd meglátjuk. Joseph voltam na csumiii

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,36 @@
 import './style.css'
 
+
 document.addEventListener('DOMContentLoaded', () => {
+    // color.html ts code
     const color = document.getElementById('color');
     const ColorInput = document.getElementById('color-input') as HTMLInputElement;
 
     if (color && ColorInput) {
         ColorInput.addEventListener('input', () => {
         color.style.backgroundColor = ColorInput.value;
+        });
+    }
+
+
+    // fuel.html ts code 
+    const selectGomb = document.getElementById('selectGomb1') as HTMLElement | null;
+    const selectText = document.getElementById('selectText') as HTMLElement | null;
+    const selectOptions = document.querySelectorAll('.option') as NodeListOf<HTMLElement>;
+
+
+    if (selectGomb && selectText && selectOptions) {
+        selectGomb.addEventListener('click', () => {
+            selectGomb.classList.toggle('active');
+        });
+
+        selectOptions.forEach(option => {
+            option.addEventListener('click', () => {
+                const text = (option.querySelector('.option-text') as HTMLElement)?.innerText;
+                if (text) {
+                    selectText.innerText = text;
+                }
+            });
         });
     }
 })
